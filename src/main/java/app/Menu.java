4dda421ddfcfacdcba684b37b8615d926a4edb9c
Lersergeevna <sergeevna.leva@gmail.com>
2026-01-17 21:34:25 +0300

    Language changed
    Загрузка Menu

--- src/main/java/app/Menu.java
+++ src/main/java/app/Menu.java
@@ -1,19 +1,20 @@
 package app;
 
 public final class Menu {
-    private Menu() {}
+    private Menu() {
+    }
 
     public static void print() {
         System.out.println("\n=== MENU ===");
-        System.out.println("1) Create/Load cars");
-        System.out.println("2) Print cars");
-        System.out.println("3) Choose sorting algorithm");
-        System.out.println("4) Sort by all 3 fields");
-        System.out.println("5) Special sort: sort EVEN by power (odd stay)");
-        System.out.println("6) Append current cars to file");
-        System.out.println("7) Count occurrences (multi-thread) by power==N");
-        System.out.println("0) Exit");
-        System.out.print("Select: ");
+        System.out.println("1) Создать/Загрузить машины");
+        System.out.println("2) Напечатать машины");
+        System.out.println("3) Выбрать алгоритм сортировки");
+        System.out.println("4) Сортировать по всем 3 полям");
+        System.out.println("5) Специальная сортировка: сортировка по четным числам в степени (нечетные числа остаются)");
+        System.out.println("6) Добавить текущие автомобили в файл");
+        System.out.println("7) Подсчет количества вхождений (многопоточный режим) по мощности");
+        System.out.println("0) Выход");
+        System.out.print("Выбрать: ");
     }
 }
 
