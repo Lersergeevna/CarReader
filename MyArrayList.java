efb513c2eaf113616e15dd3bf104688ae7360289
Sergo <gengel.ss@gmail.com>
2026-01-19 23:13:30 +0300

    +MyArrayList

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,4 +1,80 @@
-public class MyArrayList {
-	public static void main (String[] args){
-	}
+import java.util.*;
+
+public class MyArrayList<T> implements Iterable<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public void add(T item) {
+        ensureCapacity();
+        elements[size++] = item;
+    }
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        T removed = (T) elements[index];
+
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+
+        // Очищаем последний элемент (во избежание утечек)
+        elements[--size] = null;
+
+        return removed;
+    }
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elements[index];
+    }
+
+    public T set(int index, T item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T old = (T) elements[index];
+        elements[index] = item;
+        return old;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            int newCapacity = elements.length == 0 ? DEFAULT_CAPACITY : elements.length * 2;
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) elements[currentIndex++];
+            }
+        };
+    }
+    public java.util.stream.Stream<T> stream() {
+        return java.util.stream.StreamSupport.stream(this.spliterator(), false);
+    }
 }
