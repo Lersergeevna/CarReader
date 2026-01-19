6868a5e25f393fe98085bc3b841654c74564cede
MukhamedovR <clashf622@gmail.com>
2026-01-19 16:01:58 +0300

    Update Validators.java

--- Validators.java
+++ Validators.java
@@ -1,24 +1,26 @@
 public final class Validators {
     private Validators() {}
 
-    public static void validateModel(String model) {
-        if (model == null || model.strip().isEmpty()) {
-            throw new ValidationException("Ошибка: Модель не может быть пустой.");
-        }
-    }
-
     public static void validatePower(int power) {
         if (power <= 0) {
             throw new ValidationException("Ошибка: Мощность должна быть положительной.");
         }
     }
 
+    public static void validateModel(String model) {
+        if (model == null || model.strip().isEmpty()) {
+            throw new ValidationException("Ошибка: Модель не может быть пустой.");
+        }
+    }
+
     public static void validateYear(int year) {
+        // Актуально для 2026 года
         if (year <= 1886 || year > 2026) {
             throw new ValidationException("Ошибка: Год должен быть в диапазоне от 1886 до 2026.");
         }
     }
 }
+
 
 
 
