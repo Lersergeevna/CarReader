0498811bc378977ce854b81f3a47396ed0fca620
Sergo <gengel.ss@gmail.com>
2026-01-30 23:23:31 +0300

    reduction Collectors

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,48 +1,33 @@
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.stream.Collector;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class MyArrayList<T> implements Iterable<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elements;
+    private Object[] elements = new Object[DEFAULT_CAPACITY];
     private int size;
 
-    public static <T> Collector<T, ?, MyArrayList<T>> toMyArrayList() {
-        return Collector.of(MyArrayList::new, MyArrayList::add, (l, r) -> {
-            r.forEach(l::add);
-            return l;
-        });
-    }
-
-    public MyArrayList() {
-        this.elements = new Object[DEFAULT_CAPACITY];
-        this.size = 0;
-    }
-
-    private void rangeCheck(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        }
-    }
-    @SuppressWarnings("unchecked")
-    private T elementAt(int index) {
-        return (T) elements[index];
+    public int size() {
+        return size;
     }
 
     public void add(T item) {
-        ensureCapacity();
+        ensureCapacity(size + 1);
         elements[size++] = item;
     }
+
     public T remove(int index) {
         rangeCheck(index);
         T removed = elementAt(index);
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
+
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
         elements[--size] = null;
+
         return removed;
     }
+
     public T get(int index) {
         rangeCheck(index);
         return elementAt(index);
@@ -50,20 +35,34 @@ public class MyArrayList<T> implements Iterable<T> {
 
     public T set(int index, T item) {
         rangeCheck(index);
-        T old =  elementAt(index);
+        T old = elementAt(index);
         elements[index] = item;
         return old;
-      }
-
-    public int size() {
-        return size;
     }
 
-    private void ensureCapacity() {
-        if (size == elements.length) {
-            int newCapacity = elements.length == 0 ? DEFAULT_CAPACITY : elements.length * 2;
-            elements = Arrays.copyOf(elements, newCapacity);
+    public void clear() {
+        Arrays.fill(elements, 0, size, null);
+        size = 0;
+    }
+
+    public Stream<T> stream() {
+        return StreamSupport.stream(this.spliterator(), false);
+    }
+
+    private void ensureCapacity(int needed) {
+        if (needed <= elements.length) return;
+        elements = Arrays.copyOf(elements, Math.max(needed, elements.length * 2));
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Индекс: " + index + ", размер: " + size);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private T elementAt(int index) {
+        return (T) elements[index];
     }
 
     @Override
@@ -78,16 +77,9 @@ public class MyArrayList<T> implements Iterable<T> {
 
             @Override
             public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
+                if (!hasNext()) throw new NoSuchElementException();
                 return elementAt(currentIndex++);
             }
         };
-    }
-    public java.util.stream.Stream<T> stream() {
-        @SuppressWarnings("unchecked")
-        T[] arr = (T[]) Arrays.copyOf(elements, size, Object[].class);
-        return java.util.Arrays.stream(arr);
     }
 }
--- MyArrayListCollector.java
+++ MyArrayListCollector.java
@@ -1,42 +1,20 @@
-import java.util.Set;
 import java.util.stream.Collector;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
 
-public class MyArrayListCollector<T> implements Collector<T, MyArrayList<T>, MyArrayList<T>> {
+public final class MyArrayListCollector {
+    private MyArrayListCollector() {}
 
-    @Override
-    public Supplier<MyArrayList<T>> supplier() {
-        return MyArrayList::new;
-    }
+    public static <T> Collector<T, MyArrayList<T>, MyArrayList<T>> toMyArrayList() {
+        Supplier<MyArrayList<T>> supplier = MyArrayList::new;
+        BiConsumer<MyArrayList<T>, T> accumulator = MyArrayList::add;
 
-    @Override
-    public BiConsumer<MyArrayList<T>, T> accumulator() {
-        return MyArrayList::add;
-    }
-
-    @Override
-    public BinaryOperator<MyArrayList<T>> combiner() {
-        return (list1, list2) -> {
-            for (T item : list2) {
-                list1.add(item);
-            }
-            return list1;
+        BinaryOperator<MyArrayList<T>> combiner = (left, right) -> {
+            for (T item : right) left.add(item);
+            return left;
         };
-    }
 
-    @Override
-    public java.util.function.Function<MyArrayList<T>, MyArrayList<T>> finisher() {
-        return list -> list;
-    }
-
-    @Override
-    public Set<Characteristics> characteristics() {
-        return Set.of(Characteristics.IDENTITY_FINISH);
-    }
-
-    public static <T> MyArrayListCollector<T> toMyArrayList() {
-        return new MyArrayListCollector<>();
+        return Collector.of(supplier, accumulator, combiner);
     }
 }
