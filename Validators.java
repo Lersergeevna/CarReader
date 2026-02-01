f3657d551df6d553aa0af479a32ffcf9fc54293e
MukhamedovR <clashf622@gmail.com>
2026-02-01 15:22:35 +0300

    Update Validators.java

--- Validators.java
+++ Validators.java
@@ -1,25 +1,30 @@
-public final class Validators {
+import java.time.Year;
+
+public final class Validators  {
     private Validators() {}
 
     public static void validatePower(int power) {
         if (power <= 0) {
-            throw new ValidationException("Ошибка: Мощность должна быть положительной.");
+            throw new ValidationException("Мощность должна быть положительной");
         }
     }
 
     public static void validateModel(String model) {
         if (model == null || model.strip().isEmpty()) {
-            throw new ValidationException("Ошибка: Модель не может быть пустой.");
+            throw new ValidationException("Модель не может быть пустой");
         }
     }
 
     public static void validateYear(int year) {
-        // Актуально для 2026 года
-        if (year <= 1886 || year > 2026) {
-            throw new ValidationException("Ошибка: Год должен быть в диапазоне от 1886 до 2026.");
+        int maxYear = Year.now().getValue() + 1;
+        if (year < 1886 || year > maxYear) {
+            throw new ValidationException(
+                    "Год должен быть в диапазоне от 1886 до " + maxYear
+            );
         }
     }
 }
+
 
 
 
