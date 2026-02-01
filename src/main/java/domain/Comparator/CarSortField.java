9be159f2d30369e06fd0516e1f6f3c7b8f58e7fb
Mikhail Dorenskiy <michaild.1994@gmail.com>
2026-02-01 15:36:13 +0300

    Update CarSortField.java
    Убраны излишнее описание

--- src/main/java/domain/Comparator/CarSortField.java
+++ src/main/java/domain/Comparator/CarSortField.java
@@ -4,16 +4,7 @@ package domain.Comparator;
  * Поля сортировки массива автомобилей
  */
 public enum CarSortField {
-	/**
-	 * Модель
-	 */
 	MODEL, 
-	/**
-	 * Год выпуска
-	 */
 	YEAR, 
-	/**
-	 * Мощность двигателя
-	 */
 	POWER
 }
