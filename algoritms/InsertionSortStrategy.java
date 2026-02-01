cc6b402d816c68de9d950819eb268bc1cc095048
Haysboy <uchusirazvivays@gmail.com>
2026-02-01 23:13:32 +0500

    Подключил классы, добавил уникальность классу через package, CellJ переименовал в current, также в цикле while добавил прокерку, Добавил функцию получения имени.

--- algoritms/InsertionSortStrategy.java
+++ algoritms/InsertionSortStrategy.java
@@ -1,19 +1,34 @@
-// Сортировка вставками
+package sorting.algorithms;
+
+import collection.MyArrayList;
+import sorting.SortStrategy;
+
 import java.util.Comparator;
-public class InsertionSortStrategy<T> implements SortingStrategy<T> {
+
+public class InsertionSortStrategy<T> implements SortStrategy<T> {
+
     @Override
     public void sort(MyArrayList<T> list, Comparator<T> comparator) {
+        if (list == null) throw new IllegalArgumentException("Список не заполнен данными");
+        if (comparator == null) throw new IllegalArgumentException("Компаратор не получил данные из списка");
+        if (list.size() < 2) return;
+
         for (int i = 1; i < list.size(); i++) {
             T key = list.get(i);
             int j = i - 1;
-            int cellJ = list.get(j);
 
-            while (j >= 0 && comparator.compare(cellJ, key) > 0) {
-                list.set(j + 1, cellJ);
+            while (j >= 0) {
+                T current = list.get(j);
+                if (comparator.compare(current, key) <= 0) break;
+                list.set(j + 1, current);
                 j--;
             }
             list.set(j + 1, key);
-            
         }
+    }
+
+    @Override
+    public String name() {
+        return "INSERTION";
     }
 }
